821611c8c893a2ecfc44542a7f32899632a9846e
ViggyPiggy <57759856+vigneshpai2003@users.noreply.github.com>
2021-12-20 14:34:50 +0530

    added bijection

--- NewtonFractal.py
+++ NewtonFractal.py
@@ -2,28 +2,38 @@
 plots the fractal of a polynomial in complex plane
 """
 import numpy as np
+from numpy import pi
 from numpy.polynomial.polynomial import Polynomial
 from scipy.optimize import newton
 
 from matplotlib import pyplot as plt
-from matplotlib.colors import to_rgb
-
-colors = [to_rgb(c) for c in ["#6EFF6A", "#FF4178", "#5990FF"]]
+from matplotlib.cm import get_cmap
 
 
-def plotFractal(p):
-    poly = Polynomial(p)
-    roots = poly.roots()
+def fractal(f, df_dx, roots, center=0j, dims=10+10j, xres=1000, yres="auto", maxiter=10, file="Newton.png"):
+    cmap = get_cmap("viridis", len(roots))
+    colors = np.array([cmap(i) for i in range(cmap.N)])
 
-    x, y = np.meshgrid(np.linspace(-5, 5, 1000), np.linspace(-5, 5, 1000))
+    if yres == "auto":
+        yres = int(xres * dims.imag / dims.real)
+
+    x, y = np.meshgrid(np.linspace(center.real - dims.real / 2, center.real + dims.real / 2, xres),
+                       np.linspace(center.imag - dims.imag / 2, center.imag + dims.imag / 2, yres))
     mesh = x + 1j * y
 
     def color(z):
         distances = np.array([abs(r - z) for r in roots])
-        return colors[min(range(3), key=distances.__getitem__)]
+        return colors[min(range(len(roots)), key=distances.__getitem__)]
 
-    plt.imshow(np.vectorize(color, signature="()->(3)")(newton(poly, mesh, fprime=poly.deriv(), maxiter=10)))
-    plt.show()
+    plt.imsave(file, np.vectorize(color, signature="()->(4)")(newton(f, mesh, fprime=df_dx, maxiter=maxiter)))
 
 
-plotFractal([-1, 0, 0, 1])
+def polyFractal(p, *args, **kwargs):
+    poly = Polynomial(p)
+    fractal(poly, poly.deriv(), poly.roots(), *args, **kwargs)
+
+
+fractal(lambda x: np.sin(x) / x,
+        lambda x: (x * np.cos(x) - np.sin(x)) / x**2,
+        [i * pi + 0j for i in range(-5, 5 + 1, 1)],
+        dims=2+1j)
---
+++ bijection.py
@@ -0,0 +1,99 @@
+from numpy import exp, log
+import numpy as np
+
+from matplotlib import pyplot as plt
+from matplotlib.cm import get_cmap
+
+
+def get_chunks(x):
+    chunks = []
+    place = 0.1
+    chunk = []
+    for _ in range(20):
+        digit = x // place
+        x -= digit * place
+        chunk.append(digit)
+
+        if digit != 0.0:
+            chunks.append(chunk)
+            chunk = []
+
+        place *= 0.1
+
+    return chunks
+
+
+def interleave(chunk1, chunk2):
+    chunk = []
+    alter = True
+    while chunk1 and chunk2:
+        if alter:
+            chunk.append(chunk1.pop(0))
+        else:
+            chunk.append(chunk2.pop(0))
+        alter = not alter
+
+    return chunk
+
+
+def join(chunks):
+    num = 0
+    digits = []
+    for chunk in chunks:
+        digits.extend(chunk)
+
+    for i in range(len(digits)):
+        num += 10**(-i - 1) * digits[i]
+
+    return num
+
+
+def RtoUnit(x):
+    return 1 / (1 + exp(-x))
+
+
+def unitToR(x):
+    return log(x / (1 - x))
+
+
+@np.vectorize
+def f(x, y):
+    return unitToR(join(interleave(get_chunks(RtoUnit(x)), get_chunks(RtoUnit(y)))))
+
+
+def main():
+    n = 10
+    _x = np.linspace(0, 10, n)
+    _y = np.linspace(0, 10, n)
+
+    xx, yy = np.meshgrid(_x, _y, sparse=True)
+    z = f(xx, yy)
+
+    # plt.imshow(z, extent=(min(_x), max(_x), min(_y), max(_y)), origin="lower")
+    #
+    # plt.ylabel(r"$x$")
+    # plt.xlabel(r"$y$")
+    #
+    # plt.xlim([_x.min(), _x.max()])
+    # plt.ylim([_y.min(), _y.max()])
+    #
+    # plt.colorbar()
+    #
+    # plt.show()
+
+    distances = []
+    diff = []
+
+    for i in range(n):
+        for j in range(n):
+            for x in range(n):
+                for y in range(n):
+                    distances.append(((_x[i] - _x[x])**2 + (_y[j] - _y[y])**2)**0.5)
+                    diff.append(abs(z[y, x] - z[j, i]))
+
+    plt.scatter(distances, diff)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
